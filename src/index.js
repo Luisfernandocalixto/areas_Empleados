9a69a8539a85fc5f97a8445463c118a583d88c5c
“Calixto” <l19070031@huetamo.tecnm.mx>
2024-03-30 12:19:29 -0600

    modifcaciones

--- src/index.js
+++ src/index.js
@@ -43,16 +43,6 @@ app.use((req, res) => {
     res.render('./vistas/vista404.hbs');
 });
 
-app.get('/', async (req, res) => {
-    try {
-        const empleados = await pool.query(`SELECT * FROM empleados INNER JOIN departamentos ON empleados.e_id = departamentos.d_id;    `)
-        res.render('./vistas/inicio', { empleados })
-    } catch (error) {
-        console.error('Error al consultar en la base de datos:', error);
-        res.status(500).send('Error interno del servidor');
-    }
-});
-
 
 // starting the server
 app.listen(app.get('port'), () => {
